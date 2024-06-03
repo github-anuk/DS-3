0788e008164565f9a550289585bce9a9285cd407
github-anuk <160622798+github-anuk@users.noreply.github.com>
2024-06-03 17:54:55 +0200

    Add files via upload

---
+++ l-3/main.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if ( n== 1 or n== 0):
+        return 1
+ #recursive case
+ 
+    else:
+        return n*factorial(n-1)
+
+n=int(input("Enter a number: "))
+
+print("Factorial of",n,"is:",factorial(n))
